81cab8fd811ce1bbd87cd1cafbc3ffd21ea641b7
timliuGH <40328822+timliuGH@users.noreply.github.com>
2019-06-27 06:51:05 -0700

    Comment-out function to trigger mobile keyboard

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-    $('#field').click(function(e){
-        $(this).focus();
-    });
-    $('#button').click(function(e) {
-        $('#field').trigger('click');
-    });
-});
+// $(document).ready(function() {
+//     $('#field').click(function(e){
+//         $(this).focus();
+//     });
+//     $('#button').click(function(e) {
+//         $('#field').trigger('click');
+//     });
+// });
